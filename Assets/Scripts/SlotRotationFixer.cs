ada48a32d5f4137f180a404e87aa18eb6dd4c8dc
Baris123TR <93650606+Baris123TR@users.noreply.github.com>
2022-11-29 06:17:48 +0300

    Apk and Fps Test

--- Assets/Scripts/SlotRotationFixer.cs
+++ Assets/Scripts/SlotRotationFixer.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotRotationFixer : MonoBehaviour
 {
     SpinScript _spinScript;
+    public Toggle _updateToggle;
     private void Awake()
     {
         _spinScript = transform.parent.GetComponent<SpinScript>();
+        if (!_updateToggle)
+        {
+            _updateToggle = GameObject.FindWithTag("UpdateToggle").GetComponent<Toggle>();
+        }
     }
     private void Update()
     {
-        if (_spinScript.LockSlotRotationsBool)
+        if (_updateToggle.isOn)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            if (_spinScript.LockSlotRotationsBool)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
         }
     }
 }
--- Assets/Scripts/WheelLoaderScript.cs
+++ Assets/Scripts/WheelLoaderScript.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WheelLoaderScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Image _indicator;
+    public Image _wheel;
+    public Image[] _items;
 }
